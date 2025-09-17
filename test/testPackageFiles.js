50890fb41f96779537f58944990aae8fdd007438
oweitman <oweitman@gmx.de>
2025-09-17 02:09:03 +0200

    try to fix test

---
+++ test/testPackageFiles.js
@@ -0,0 +1,17 @@
+/* jshint -W097 */
+/* jshint strict:false */
+/* jslint node: true */
+/* jshint expr: true */
+'use strict';
+
+const expect = require('chai').expect;
+const fs = require('fs');
+
+describe('Test package.json and io-package.json', () => {
+    it('Test package files', done => {
+        console.log();
+
+
+        done();
+    });
+});
